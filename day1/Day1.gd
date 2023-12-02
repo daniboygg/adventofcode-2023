b424b1647002698f34913ea31470020d72f5ef54
daniboygg <danigayosog@gmail.com>
2023-12-02 17:34:06 +0100

    Trim spaces

--- day1/Day1.gd
+++ day1/Day1.gd
@@ -14,30 +14,30 @@ func first(content: String):
 	var split := content.split("\n")
 	var number_regex := RegEx.new()
 	number_regex.compile("\\d")
-	
+
 	var final_result := 0
-	
+
 	for line in split:
 		if line == "":
 			continue
 		var result := number_regex.search(line)
 		var first_number := result.get_string()
-		
+
 		result = number_regex.search(reverse_line(line))
 		var last_number := result.get_string()
-		
+
 		var number := int(first_number + last_number)
 		final_result += number
-		
+
 	print("Result of day 1-1: %d" % final_result)
-	
+
 
 func second(content: String):
 	var split := content.split("\n")
 	var number_regex := RegEx.new()
 	# hacky solution get reverse numbers for identify in reverse line
 	number_regex.compile("(one|two|three|four|five|six|seven|eight|nine|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|\\d)")
-	
+
 	var lookup_number = {
 		"one": "1",
 		"two": "2",
@@ -59,23 +59,23 @@ func second(content: String):
 		"thgie": "8",
 		"enin": "9",
 	}
-	
+
 	var final_result := 0
-	
+
 	for line in split:
 		if line == "":
 			continue
 		var result := number_regex.search(line)
 		var result_string := result.get_string()
 		var first_number: String = lookup_number.get(result_string, result_string)
-		
+
 		result = number_regex.search(reverse_line(line))
 		result_string = result.get_string()
 		var last_number: String = lookup_number.get(result_string, result_string)
-		
+
 		var number := int(first_number + last_number)
 		final_result += number
-	
+
 	print("Result of day 1-2: %d" % final_result)
 
 
--- day2/Day2.gd
+++ day2/Day2.gd
@@ -13,18 +13,18 @@ func _ready():
 	var valid_games := 0
 	var sum_of_powers := 0
 
-	for line: String in lines: 
+	for line: String in lines:
 		var split := line.split(":")
 		var game := split[0]
 		var plays := split[1].split(";")
-		
+
 		var cube_count = {
 			"blue": 0,
 			"red": 0,
 			"green": 0,
 		}
-		
-		for play_set in plays: 
+
+		for play_set in plays:
 			var reveals := play_set.split(",")
 			for reveal in reveals:
 				reveal = reveal.strip_edges()
@@ -36,17 +36,17 @@ func _ready():
 						cube_count["blue"] = max(cube_count["blue"], cube_number)
 					"red":
 						cube_count["red"] = max(cube_count["red"], cube_number)
-					"green": 
+					"green":
 						cube_count["green"] = max(cube_count["green"], cube_number)
-					var error: 
+					var error:
 						assert(false, "Invalid value %s" % error)
-						
+
 		var game_id := int(game.split(" ")[1])
 		if cube_count["red"] <= 12 and  cube_count["green"] <= 13 and  cube_count["blue"] <= 14:
 			valid_games += game_id
-			
+
 		var power = cube_count["red"] * cube_count["green"] * cube_count["blue"]
 		sum_of_powers += power
-	
+
 	print("Result of day 2-1: %d" % valid_games)
 	print("Result of day 2-2: %d" % sum_of_powers)
